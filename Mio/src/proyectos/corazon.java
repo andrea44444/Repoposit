b08813df682111d34e9808877761840544f621eb
Andrea Cabrera <andreaolcakova@gmail.com>
2021-12-21 13:52:35 +0100

    MENSAJES2

---
+++ Mio/src/proyectos/corazon.java
@@ -0,0 +1,20 @@
+package proyectos;
+
+public class corazon {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\n    ,d88b.d88b, \r\n"
+				+ "    8888A+C8888\r\n"
+				+ "    'Y8888888Y'\r\n"
+				+ "      `Y888Y'\r\n"
+				+ "        `Y'");
+		
+		System.out.println("\n    ,d''b.d''b, \r\n"
+				+ "    8         8\r\n"
+				+ "    'Y       Y'\r\n"
+				+ "      `Y   Y'\r\n"
+				+ "        `Y'");
+	}
+
+}
